d379e3a47e120646fa6375de7c2319e2d5e454f2
Robin Hopkins <robinjohnhopkins@hotmail.com>
2019-04-18 10:47:32 +0100

    added d3 page simple functionality that doesn't get in way of react

--- src/pages/D3Page.js
+++ src/pages/D3Page.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Row, Col } from 'reactstrap';
 
 import Page from 'components/Page';
 
 import { iconD3Data, numberD3Data } from 'demos/D3Page';
+import Viz from 'demos/Viz.js'
 
-const D3Page = () => {
-  return (
-    <Page
-      className="D3Page"
-      title="D3s"
-      breadcrumbs={[{ name: 'D3s', active: true }]}
-    >
-      <Row>
-        {numberD3Data.map(({ color }, index) => (
-          <Col key={index} lg={4} md={6} sm={6} xs={12} className="mb-3">
-           
-          </Col>
-        ))}
-      </Row>
+export default class D3Page extends Component {
+  state = {
+	  color: "", 
+	  width: "", 
+	  toDraw: [], 
+  }
+  onSubmit = (evt) => {
+  	evt.preventDefault(); 
+  	const newShape = {
+  	   color: this.state.color, 
+  	   width: this.state.width,
+  	}
+    this.setState({ toDraw: [...this.state.toDraw, newShape]})
+  } 
 
-      <Row>
-        {iconD3Data.map(
-          ({ bgColor, icon, title, subtitle, ...restProps }, index) => (
-            <Col key={index} lg={4} md={6} sm={6} xs={12} className="mb-3">
-            
-            </Col>
-          )
-        )}
-      </Row>
-    </Page>
-  );
+  onChange = (evt) => {
+  	this.setState({[evt.target.name]: evt.target.value})
+  }
+  render() {
+    return (
+      <Page
+        className="D3Page"
+        title="D3s"
+        breadcrumbs={[{ name: 'D3s', active: true }]}
+      >
+      <div className="controller">
+        <form onSubmit={this.onSubmit}>
+        <label htmlFor="colorSelect">pick a color:</label>
+        <select id="colorSelect" name="color" onChange={this.onChange} value={this.state.color||"default"}>
+          <option disabled value="default">choose</option>
+          <option value="red">red</option>
+          <option value="orange">orange</option>
+          <option value="yellow">yellow</option>
+        </select>
+        <label htmlFor="pixelInput">how big:</label>
+        <input id="pixelInput" name="width" onChange={this.onChange} />
+        <button type="submit">draw!</button>
+        </form>
+        { this.state.toDraw.length ? <Viz shapes={this.state.toDraw}/> : null}
+      </div>
+      </Page>
+    );
+  }
 };
-
-export default D3Page;
